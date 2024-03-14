5d6cc9d6461051ec869bbb7fc0f5bd047549e05e
adamw <adam@warski.org>
2024-03-14 13:53:27 +0100

    WIP

--- core/src/main/scala/scalar/observability/Tracing.scala
+++ core/src/main/scala/scalar/observability/Tracing.scala
@@ -2,12 +2,12 @@ package scalar.observability
 
 import io.opentelemetry.api.trace.{Span, SpanBuilder}
 import io.opentelemetry.context.Context
-import ox.ForkLocal
+import ox.{ForkLocal, Ox}
 
 object Tracing:
   private val currentSpan = ForkLocal[Option[Span]](None)
 
-  def withSpan[T](spanBuilder: SpanBuilder)(f: => T): T =
+  def withSpan[T](spanBuilder: SpanBuilder)(f: Ox ?=> T): T =
     currentSpan.get().foreach(parent => spanBuilder.setParent(Context.current().`with`(parent)))
 
     val span = spanBuilder.startSpan()
